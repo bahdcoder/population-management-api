f7224c490d3a51bc9fd8a930e70228ba4187f239
Kati Frantz <bahdcoder@gmail.com>
2019-02-10 19:29:37 +0100

    add total computed property

--- app/Models/Location.js
+++ app/Models/Location.js
@@ -3,7 +3,25 @@
 const Model = use('Model')
 
 class Location extends Model {
+  /**
+   * Get all computed properties
+   * 
+   * @return {Array}
+   */
+  static get computed () {
+    return ['total_count']
+  }
 
+  /**
+   * Get the total number of residents in this location
+   * 
+   * @param {Object} model
+   * 
+   * @return {Integer} 
+   */
+  getTotalCount({ male_count, female_count }) {
+    return male_count + female_count
+  }
   /**
    * A location can have many sub locations
    * 
